ecd7ca4339076b400e36bd6e0f9cb26ebd7e17c5
Shadab Qamar <shadabqamar94@gmail.com>
2017-11-22 00:27:59 +0530

    using equals method to comapre objects

--- DemoBuidingFloorProject/src/main/java/org/shadab/demo/configuration/BeanScopeApp.java
+++ DemoBuidingFloorProject/src/main/java/org/shadab/demo/configuration/BeanScopeApp.java
@@ -9,18 +9,17 @@ public class BeanScopeApp {
 		// load the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"bean-scope-applicationContext.xml");
-		
-		//retrieve the spring bean
-		Building floor=context.getBean("firstFloor",Building.class);
-		System.out.println("Retrieving First bean with memory location: "+floor);
-		
-		Building singletonfloor=context.getBean("firstFloor",Building.class);
-		System.out.println("Retrieving Second bean with memory location : "+singletonfloor);
 
-		
-		//checking if beans are singleton
-		boolean result=(floor == singletonfloor);
-		System.out.println("\nIs the bean singleton? Answer :" +result);
+		// retrieve the spring bean
+		Building floor = context.getBean("firstFloor", Building.class);
+		System.out.println("Retrieving First bean with memory location: " + floor);
+
+		Building singletonfloor = context.getBean("firstFloor", Building.class);
+		System.out.println("Retrieving Second bean with memory location : " + singletonfloor);
+
+		// checking if beans are singleton
+		boolean result = (floor.equals(singletonfloor));
+		System.out.println("\nIs the bean singleton? Answer :" + result);
 		context.close();
 
 	}
