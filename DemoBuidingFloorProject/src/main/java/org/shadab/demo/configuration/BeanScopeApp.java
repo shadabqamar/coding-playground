f779735f1c49f32301b96fe2557b8152e02945da
Shadab Qamar <shadabqamar94@gmail.com>
2017-11-22 00:08:24 +0530

    Bean scope test with prototype and singleton

---
+++ DemoBuidingFloorProject/src/main/java/org/shadab/demo/configuration/BeanScopeApp.java
@@ -0,0 +1,28 @@
+package org.shadab.demo.configuration;
+
+import org.shadab.demo.springioc.Building;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeApp {
+
+	public static void main(String args[]) {
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"bean-scope-applicationContext.xml");
+		
+		//retrieve the spring bean
+		Building floor=context.getBean("firstFloor",Building.class);
+		System.out.println("Retrieving First bean with memory location: "+floor);
+		
+		Building singletonfloor=context.getBean("firstFloor",Building.class);
+		System.out.println("Retrieving Second bean with memory location : "+singletonfloor);
+
+		
+		//checking if beans are singleton
+		boolean result=(floor == singletonfloor);
+		System.out.println("\nIs the bean singleton? Answer :" +result);
+		context.close();
+
+	}
+
+}
